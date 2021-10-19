84b7d20691a3d8fce9050d5eea78e037794c38bd
tsfong <90808914+tsfong@users.noreply.github.com>
2021-10-19 16:41:14 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="film-library">
+          <div className="film-list">
+            <h1 className="section-title">FILMS</h1>
+          </div>
+          <div className="film-details">
+            <h1 className="section-title">DETAILS</h1>
+          </div>
+      </div>
+    )
+}
+
+export default App
